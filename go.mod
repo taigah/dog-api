59cec0d199ad3f05d14b0b0cc89360d3bd512360
Taigah <taigah.dev@gmail.com>
2021-02-14 04:00:58 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/taigah/dog-api
+
+go 1.15
+
+require github.com/stretchr/testify v1.7.0
---
+++ pkg/api/api.go
@@ -0,0 +1,231 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func unmarshalFromURL(url string, data interface{}) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetAllBreeds() ([]string, error) {
+	res, err := http.Get("https://dog.ceo/api/breeds/list/all")
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		Breeds map[string][]string `json:"message"`
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	breeds := make([]string, 0)
+
+	for breed, subBreeds := range data.Breeds {
+		if len(subBreeds) == 0 {
+			breeds = append(breeds, breed)
+		} else {
+			for _, subBreed := range subBreeds {
+				breeds = append(breeds, fmt.Sprintf("%v %v", subBreed, breed))
+			}
+		}
+	}
+
+	return breeds, nil
+}
+
+func GetRandomDogImageUrl() (string, error) {
+	res, err := http.Get("https://dog.ceo/api/breeds/image/random")
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var data struct {
+		URL string `json:"message"`
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return "", err
+	}
+
+	return data.URL, nil
+}
+
+func GetBunchOfRandomDogImagesUrl(imageCount int) ([]string, error) {
+	res, err := http.Get(fmt.Sprintf("https://dog.ceo/api/breeds/image/random/%v", imageCount))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		URLs []string `json:"message"`
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.URLs, nil
+}
+
+func GetAllDogsOfBreed(breed string) ([]string, error) {
+	res, err := http.Get(fmt.Sprintf("https://dog.ceo/api/breed/%v/images", breed))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		URLs []string `json:"message"`
+	}
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data.URLs, nil
+}
+
+func GetRandomDogOfBreed(breed string) (string, error) {
+	res, err := http.Get(fmt.Sprintf("https://dog.ceo/api/breed/%v/images/random", breed))
+	if err != nil {
+		return "", err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var data struct {
+		URL string `json:"message"`
+	}
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		return "", err
+	}
+
+	return data.URL, nil
+}
+
+func GetBunchOfRandomDogsOfBreed(breed string, imageCount int) ([]string, error) {
+	res, err := http.Get(fmt.Sprintf("https://dog.ceo/api/breed/%v/images/random/%v", breed, imageCount))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		URLs []string `json:"message"`
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.URLs, nil
+}
+
+func GetSubBreeds(breed string) ([]string, error) {
+	res, err := http.Get(fmt.Sprintf("https://dog.ceo/api/breed/%v/list", breed))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		SubBreeds []string `json:"message"`
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.SubBreeds, nil
+}
+
+func GetDogsOfSubBreed(breed string, subBreed string) ([]string, error) {
+	var data struct {
+		URLs []string `json:"message"`
+	}
+	err := unmarshalFromURL(fmt.Sprintf("https://dog.ceo/api/breed/%v/%v/images", breed, subBreed), &data)
+	if err != nil {
+		return nil, err
+	}
+	return data.URLs, nil
+}
+
+func GetRandomDogOfSubBreed(breed string, subBreed string) (string, error) {
+	var data struct {
+		URL string `json:"message"`
+	}
+	err := unmarshalFromURL(fmt.Sprintf("https://dog.ceo/api/breed/%v/%v/images/random", breed, subBreed), &data)
+	if err != nil {
+		return "", err
+	}
+	return data.URL, nil
+}
+
+func GetBunchOfRandomDogsOfSubBreed(breed string, subBreed string, imageCount int) ([]string, error) {
+	var data struct {
+		URLs []string `json:"message"`
+	}
+	err := unmarshalFromURL(fmt.Sprintf("https://dog.ceo/api/breed/%v/%v/images/random/%v", breed, subBreed, imageCount), &data)
+	if err != nil {
+		return nil, err
+	}
+	return data.URLs, nil
+}
