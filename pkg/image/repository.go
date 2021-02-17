39dee1fc49a7d152ed414bb8d16db522b79f435a
Taigah <taigah.dev@gmail.com>
2021-02-17 16:50:21 +0100

    feat(image): add GetBunchRandoms

--- pkg/image/repository.go
+++ pkg/image/repository.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	"fmt"
 	"net/http"
 
 	ihttp "github.com/taigah/dog-api/internal/http"
@@ -8,6 +9,7 @@ import (
 
 type ImageRepository interface {
 	GetRandom() (Image, error)
+	GetBunchRandoms(imageCount int) ([]Image, error)
 }
 
 type imageRepositoryImpl struct {
@@ -20,7 +22,7 @@ func NewImageRepository(client *http.Client) ImageRepository {
 
 func (rep imageRepositoryImpl) GetRandom() (Image, error) {
 	var data struct {
-		Image string `json:"message"`
+		Image Image `json:"message"`
 	}
 	err := ihttp.UnmarshalFromURL(rep.client, "https://dog.ceo/api/breeds/image/random", &data)
 
@@ -29,4 +31,17 @@ func (rep imageRepositoryImpl) GetRandom() (Image, error) {
 	}
 
 	return data.Image, nil
+}
+
+func (rep imageRepositoryImpl) GetBunchRandoms(imageCount int) ([]Image, error) {
+	var data struct {
+		Images []Image `json:"message"`
+	}
+	err := ihttp.UnmarshalFromURL(rep.client, fmt.Sprintf("https://dog.ceo/api/breeds/image/random/%v", imageCount), &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data.Images, nil
 }
--- pkg/image/repository_test.go
+++ pkg/image/repository_test.go
@@ -14,3 +14,14 @@ func Test_Get_Random_Should_Return_A_Random_Image(t *testing.T) {
 	assert.Nil(t, err)
 	assert.True(t, strings.HasPrefix(image, "http"))
 }
+
+func Test_GetBunchRandoms_Should_Return_A_Bunch_Of_Random_Images(t *testing.T) {
+	rep := NewImageRepository(http.DefaultClient)
+	imageCount := 30
+	images, err := rep.GetBunchRandoms(imageCount)
+	assert.Nil(t, err)
+	assert.Len(t, images, imageCount)
+	for _, image := range images {
+		assert.True(t, strings.HasPrefix(image, "http"))
+	}
+}
